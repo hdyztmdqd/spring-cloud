8734aa082eb043ae978f4c5f04327ce443c34f99
hdy <zhhdys295@163.com>
2017-11-25 14:12:49 +0800

    commit

---
+++ web-service1/src/main/java/com/example/webservice1/WebService1Application.java
@@ -0,0 +1,12 @@
+package com.example.webservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebService1Application.class, args);
+	}
+}
