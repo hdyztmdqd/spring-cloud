0a4c7621860a144d847ed4928d47269a19d8de5d
hdy <zhhdys295@163.com>
2017-11-25 17:20:54 +0800

    init architecture

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,12 +1,21 @@
 package com.example.apigateway;
 
+import com.example.apigateway.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringCloudApplication
+@EnableZuulProxy
 public class ApiGatewayApplication {
+//    @Bean
+    AccessFilter accessFilter() {
+	    return new AccessFilter();
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }
