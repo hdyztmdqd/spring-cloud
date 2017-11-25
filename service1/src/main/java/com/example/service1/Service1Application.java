a657e593c18969e800c03e7d828505cb705e9314
hdyztmdqd <zhhdys295@163.com>
2017-11-25 13:12:10 +0800

    激活自动配置

--- service1/src/main/java/com/example/service1/Service1Application.java
+++ service1/src/main/java/com/example/service1/Service1Application.java
@@ -1,9 +1,11 @@
 package com.example.service1;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Service1Application {
 
 	public static void main(String[] args) {
